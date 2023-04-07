4b5dca581caba69488c6a35fffa9455e2c0ac150
jspoh <jingsengyeah@gmail.com>
2023-04-07 22:15:15 +0800

    added global exception handling

---
+++ crud/src/main/java/com/springboot/crud/rest/TodoExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.springboot.crud.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class TodoExceptionHandler {
+  // handle exceptions of type TodoNotFoundException.
+  @ExceptionHandler
+  public ResponseEntity<TodoErrorResponse> handleException(TodoNotFoundException err) {
+    TodoErrorResponse res = new TodoErrorResponse(HttpStatus.NOT_FOUND.value(), err.getMessage(), System.currentTimeMillis());
+//    res.setStatus(HttpStatus.NOT_FOUND.value());
+//    res.setMessage(err.getMessage());
+//    res.setTimeStamp(System.currentTimeMillis());
+
+    return new ResponseEntity<>(res, HttpStatus.NOT_FOUND);
+  }
+
+  // handle all other exceptions (see parameter of Exception type catches all exceptions)
+  @ExceptionHandler
+  public ResponseEntity<TodoErrorResponse> handleException(Exception err) {
+    TodoErrorResponse res = new TodoErrorResponse(HttpStatus.BAD_REQUEST.value(), err.getMessage(), System.currentTimeMillis());
+
+    return new ResponseEntity<>(res, HttpStatus.BAD_REQUEST);
+  }
+}
